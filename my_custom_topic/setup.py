d20e3ac285553adfef820a6ca4277fac9a18e086
minoru akahori <minoruaka@gmail.com>
2022-09-15 15:59:47 +0900

    update

--- my_custom_topic/setup.py
+++ my_custom_topic/setup.py
@@ -1,25 +1,26 @@
 from setuptools import setup
 
-package_name = 'my_custom_topic'
+package_name = "my_custom_topic"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='ubuntu',
-    maintainer_email='ubuntu@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="ubuntu",
+    maintainer_email="ubuntu@todo.todo",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            "my_publisher_node = my_topic.my_publisher_node:main",
+            "my_subscriber_node = my_topic.my_subscriber_node:main",
         ],
     },
 )
